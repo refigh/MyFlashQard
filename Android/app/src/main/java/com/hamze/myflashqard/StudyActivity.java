1d759daf1df085a9c1636bd83c686ffc625474bf
hamze <hamze60@gmail.com>
2018-11-04 22:26:00 +0300

    Release 1.23: tabs with a content are underlined

--- Android/app/src/main/java/com/hamze/myflashqard/StudyActivity.java
+++ Android/app/src/main/java/com/hamze/myflashqard/StudyActivity.java
@@ -3,16 +3,16 @@ package com.hamze.myflashqard;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.text.Html;
-import android.text.InputType;
 import android.text.Spanned;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Gravity;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -28,9 +28,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-
-import static com.hamze.myflashqard.R.drawable.comment;
 
 
 /**
@@ -65,9 +62,9 @@ public class StudyActivity extends Activity {
 
     private flashcard_collectin my_fc_col;
 
-    //info of last showed card
-    private vocabulary_card Lastcard;
-    private int Lastcard_stage_num;
+    //info of last shown card
+    private vocabulary_card cur_card;
+    private int cur_stage_id;
 
     //date format
     SimpleDateFormat df = new SimpleDateFormat("d.M.yyyy");
@@ -89,6 +86,7 @@ public class StudyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
 
+
         //show button
         button_show = (Button) findViewById(R.id.button_show);
         button_show.setOnClickListener(button_show_OnClickListener);
@@ -106,12 +104,10 @@ public class StudyActivity extends Activity {
         imageButton_comment.setOnClickListener(imageButton_comment_OnClickListener);
 
 
-        //text boxes
+        //text boxes. side 1 and side 2
         textView_side1 = (TextView) findViewById(R.id.textView_side1);
         textView_side1.setMovementMethod(new ScrollingMovementMethod());
 
-
-        //text boxes
         textView_side2 = (TextView) findViewById(R.id.textView_side2);
         textView_side2.setMovementMethod(new ScrollingMovementMethod());
 
@@ -134,13 +130,13 @@ public class StudyActivity extends Activity {
 
         //Rating bar
         ratingBar_NumCorrect = (RatingBar) findViewById(R.id.ratingBar_NumCorrect);
-        ratingBar_NumCorrect.setNumStars(my_fc_col.getMaxStageNum() - 2); //number of times that a card is answerd correctly. including the final stage.
+        ratingBar_NumCorrect.setNumStars(my_fc_col.getMaxStageNum() - 2); //number of times that a card is answered correctly. including the final stage.
         ratingBar_NumCorrect.setRating(0);
         ratingBar_NumCorrect.setStepSize(1);
 
         //info of last showed card
-        Lastcard = null;
-        Lastcard_stage_num = -1;
+        cur_card = null;
+        cur_stage_id = -1;
 
         //access to the unique flashcard collection.
         my_fc_col = MainActivity.getFlashcard();
@@ -149,37 +145,42 @@ public class StudyActivity extends Activity {
         tabHost = (TabHost) findViewById(android.R.id.tabhost);
         tabHost.setup();
 
-        TabWidget tabs = tabHost.getTabWidget();
-        FrameLayout tabContent = tabHost.getTabContentView();
-        // Get the original tab textviews and remove them from the viewgroup.
-        TextView[] originalTextViews = new TextView[tabs.getTabCount()];
-        for (int index = 0; index < tabs.getTabCount(); index++)
-            originalTextViews[index] = (TextView) tabs.getChildTabViewAt(index);
-        tabs.removeAllViews();
-        // Ensure that all tab content children are not visible at startup.
-        for (int index = 0; index < tabContent.getChildCount(); index++)
-            tabContent.getChildAt(index).setVisibility(View.GONE);
+        TabWidget tab_widgets = tabHost.getTabWidget();
+        FrameLayout tab_contents = tabHost.getTabContentView();
+
+        // Get the original tab titles and remove them from the viewgroup.
+        TextView[] original_titles = new TextView[tab_widgets.getTabCount()];
+        for (int index = 0; index < tab_widgets.getTabCount(); index++)
+            original_titles[index] = (TextView) tab_widgets.getChildTabViewAt(index);
+        tab_widgets.removeAllViews();
+
+        // Ensure that all tab contents are not visible at startup.
+        for (int index = 0; index < tab_contents.getChildCount(); index++)
+            tab_contents.getChildAt(index).setVisibility(View.GONE);
+
         // Create the tabspec based on the textview children in the xml file.
         // Or create simple tabspec instances in any other way...
-        for (int index = 0; index < originalTextViews.length; index++) {
-            final TextView curtab_WTV = originalTextViews[index];
-            final View curtab_CV = tabContent.getChildAt(index);
-            //set tag, from widget list
-            TabHost.TabSpec curtab_Spec = tabHost.newTabSpec((String) curtab_WTV.getTag());
-            //set content from content list
+        for (int index = 0; index < original_titles.length; index++) {
+            final TextView curtab_title = original_titles[index];
+            final View curtab_content = tab_contents.getChildAt(index);
+
+            //set tab tag
+            TabHost.TabSpec curtab_Spec = tabHost.newTabSpec((String) curtab_title.getTag());
+            //set content
             curtab_Spec.setContent(new TabHost.TabContentFactory() {
                 @Override
                 public View createTabContent(String tag) {
-                    return curtab_CV;
+                    return curtab_content;
                 }
             });
-            if (curtab_WTV.getBackground() == null)
-                curtab_Spec.setIndicator(curtab_WTV.getText());
+            //set tab title
+            if (curtab_title.getBackground() == null)
+                curtab_Spec.setIndicator(curtab_title.getText());
             else
-                curtab_Spec.setIndicator(curtab_WTV.getText(), curtab_WTV.getBackground());
+                curtab_Spec.setIndicator(curtab_title.getText(), curtab_title.getBackground());
+
             tabHost.addTab(curtab_Spec);
         } // for
-
 
         //Today
         cal = Calendar.getInstance();
@@ -200,7 +201,7 @@ public class StudyActivity extends Activity {
     final View.OnClickListener button_close_study_OnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(final View v) {
-            //study session is clossed, but the current state of my_fc_col is not touched.
+            //study session is closed, but the current state of my_fc_col is not touched.
 
             my_fc_col.update_card_count();
 
@@ -251,67 +252,79 @@ public class StudyActivity extends Activity {
             String string_comment = "";
             String string_synonym = "";
             String string_antonym = "";
-            int next_stage = -1;
+            int dest_stage_id = -1;
             ratingBar_NumCorrect.setRating(0);
 
 
             // Before showing next card, move the currently shown card to proper stage.
-            // in 2 situations there is no previous card: before first card, and after the last card (when there is no card to study)
-            if (Lastcard != null) {
+            // in 2 situations there is no previous card: before first card, and after the last card 
+            // (when there is no card to study)
+            if (cur_card != null) {
 
                 //in current algorithm we only care about last answering info.
-                Lastcard.The_statistics.answer_date.clear();
-                Lastcard.The_statistics.answer_value.clear();
+                cur_card.The_statistics.answer_date.clear();
+                cur_card.The_statistics.answer_value.clear();
 
                 // correct card should be moved to next stage.
                 // Wrong card should be moved to start stage.
                 if (checkBox_correct.isChecked()) {  //correct answer
 
-                    Lastcard.The_statistics.answer_value.add("true");
-                    if (Lastcard_stage_num < (my_fc_col.getMaxStageNum() - 1))
-                        next_stage = Lastcard_stage_num + 1; // go next
+                    cur_card.The_statistics.answer_value.add("true");
+                    if (cur_stage_id < (my_fc_col.getMaxStageNum() - 1))
+                        dest_stage_id = cur_stage_id + 1; // go next
                     else
-                        next_stage = Lastcard_stage_num; //stay in final stage
+                        dest_stage_id = cur_stage_id; //stay in final stage
 
                 } else { //wrong answer
-                    Lastcard.The_statistics.answer_value.add("false");
-                    next_stage = 1;
+                    cur_card.The_statistics.answer_value.add("false");
+                    dest_stage_id = 1;
                 }
-                Lastcard.The_statistics.answer_date.add(today_formated);
+                cur_card.The_statistics.answer_date.add(today_formated);
 
 
-                //move it to next stage
-                my_fc_col.stage_list[Lastcard_stage_num].cards.remove(Lastcard); // must return true...
-                my_fc_col.stage_list[next_stage].cards.addLast(Lastcard);
-                my_fc_col.stage_list[next_stage].stage_type = 1; //active stage, if it is not.
+                //move it to dest stage
+                my_fc_col.stage_list[cur_stage_id].cards.remove(cur_card); // must return true...
+                my_fc_col.stage_list[dest_stage_id].cards.addLast(cur_card);
+                my_fc_col.stage_list[dest_stage_id].stage_type = 1; //active stage, if it is not.
 
             } //manage previously shown card
 
 
             //select next card
-            Lastcard_stage_num = my_fc_col.getMaxStageNum() - 2; // no card is selected from Final stage. then search is started from stage before to last
+            // no card will be selected from Final stage. then search is started from stage before to last
+            cur_stage_id = my_fc_col.getMaxStageNum() - 2;
             boolean not_found = true;
             while (true) { //not_found) {
-                stage Lastcard_stage = my_fc_col.stage_list[Lastcard_stage_num];
+                stage cur_stage = my_fc_col.stage_list[cur_stage_id];
 
-                if (Lastcard_stage_num < 1) //card not found
+                //card not found
+                if (cur_stage_id < 1)
                     break;
-                else if ((Lastcard_stage.stage_type == -1) || Lastcard_stage.cards.isEmpty()) { //skip inactive or empty stages
-                    Lastcard_stage_num--;
+
+                //skip inactive or empty stages
+                else if ((cur_stage.stage_type == -1) || cur_stage.cards.isEmpty()) {
+                    cur_stage_id--;
                     continue;
-                } else if ((Lastcard_stage_num == 1) || (Lastcard_stage.cards.getFirst().The_statistics.answer_date.isEmpty())) {
-                    //when head has no date, it should not happen in any stage other than start stage, then we assume we are in stage 1
-                    // from stage 1 (fresh cards) select randomly,
+                }
+
+                // select next card from first stage:
+                // when head has no date, it should not happen in any stage other than start stage,
+                // then we assume we are in stage 1. From stage 1 (fresh cards) select randomly,
+                else if ((cur_stage_id == 1) || (cur_stage.cards.getFirst().The_statistics.answer_date.isEmpty())) {
                     not_found = false;
                     rand = new Random();
-                    int index = rand.nextInt(Lastcard_stage.cards.size()); // 0 to card_num -1
-                    Lastcard = Lastcard_stage.cards.get(index);
+                    int index = rand.nextInt(cur_stage.cards.size()); // 0 to card_num -1
+                    cur_card = cur_stage.cards.get(index);
                     break;
-                } else {
+                }
+
+                // select next card from stage_num > 1
+                else {
                     // find time difference with head of current stage
+                    // note that, head of state, is oldest card.
                     Date head_date = null;
                     try {
-                        head_date = df.parse(Lastcard_stage.cards.getFirst().The_statistics.answer_date.getFirst());
+                        head_date = df.parse(cur_stage.cards.getFirst().The_statistics.answer_date.getFirst());
                     } catch (ParseException e) {
                         //TODO: assign new type of error due to time format error
                         e.printStackTrace();
@@ -321,13 +334,14 @@ public class StudyActivity extends Activity {
                     long t2 = head_date.getTime();
                     long time_dif_milisec = t1 - t2;
 
-                    if (time_dif_milisec > my_fc_col.getMIN_REVIEW_TIME(Lastcard_stage_num)) { // cards can not be reviewed earlier than one day
-                        Lastcard = Lastcard_stage.cards.getFirst();
+                    // cards can not be reviewed earlier than one day
+                    if (time_dif_milisec > my_fc_col.getMIN_REVIEW_TIME(cur_stage_id)) {
+                        cur_card = cur_stage.cards.getFirst();
                         not_found = false;
                         break;
                     } else {
-                        //skip this stage, since it's head (and then whole cards) is not old enough
-                        Lastcard_stage_num--;
+                        //skip this stage, since it's head (and then rest ofs cards) is not old enough
+                        cur_stage_id--;
                     }
                 }
             }//while - search for next card
@@ -335,8 +349,8 @@ public class StudyActivity extends Activity {
 
             // give value to strings and convert them to HTML format
             if (not_found) {
-                Lastcard = null;
-                Lastcard_stage_num = -1;
+                cur_card = null;
+                cur_stage_id = -1;
                 string_side1 = "فعلا کارتی موجود نیست.";
                 string_side2 = "";
                 string_examp = "";
@@ -345,14 +359,14 @@ public class StudyActivity extends Activity {
                 string_antonym = "";
                 ratingBar_NumCorrect.setRating(0);
             } else {
-                string_side1 = Lastcard.Text_of_First_Language;
-                string_side2 = Lastcard.Text_of_Second_Language;
-                string_examp = Lastcard.Text_of_examples;
-                string_comment = Lastcard.Text_of_comments;
-                string_synonym = Lastcard.Text_of_synonyms;
-                string_antonym = Lastcard.Text_of_antonyms;
+                string_side1 = cur_card.Text_of_First_Language;
+                string_side2 = cur_card.Text_of_Second_Language;
+                string_examp = cur_card.Text_of_examples;
+                string_comment = cur_card.Text_of_comments;
+                string_synonym = cur_card.Text_of_synonyms;
+                string_antonym = cur_card.Text_of_antonyms;
                 // set how many times the card is answered correctly
-                ratingBar_NumCorrect.setRating(Lastcard_stage_num - 1);
+                ratingBar_NumCorrect.setRating(cur_stage_id - 1);
             }
 
             //convert above string HTML format
@@ -391,6 +405,33 @@ public class StudyActivity extends Activity {
             textView_side2.setVisibility(TextView.INVISIBLE);
             tabHost.getTabContentView().setVisibility(FrameLayout.INVISIBLE);
 
+
+
+            //make the title of non-empty texts, as underlined.
+            TabWidget tab_widgets = tabHost.getTabWidget();
+            for (int index = 0; index < tab_widgets.getChildCount(); index++) {
+                TextView tv =  (TextView)tab_widgets.getChildAt(index).findViewById(android.R.id.title);
+                tv.setPaintFlags(tv.getPaintFlags() & ~Paint.UNDERLINE_TEXT_FLAG); // remove underline
+            }
+            if  (!string_examp.isEmpty()) {
+                TextView tv =  (TextView)tab_widgets.getChildAt(0).findViewById(android.R.id.title);
+                tv.setPaintFlags(tv.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG); // add underline
+            }
+            if (!string_comment.isEmpty()){
+                TextView tv =  (TextView)tab_widgets.getChildAt(1).findViewById(android.R.id.title);
+                tv.setPaintFlags(tv.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG); // add underline
+            }
+            if (!string_synonym.isEmpty()){
+                TextView tv =  (TextView)tab_widgets.getChildAt(2).findViewById(android.R.id.title);
+                tv.setPaintFlags(tv.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG); // add underline
+            }
+            if (!string_antonym.isEmpty()){
+                TextView tv =  (TextView)tab_widgets.getChildAt(3).findViewById(android.R.id.title);
+                tv.setPaintFlags(tv.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG); // add underline
+            }
+
+
+
             return;
 
         } //onClick
@@ -406,7 +447,7 @@ public class StudyActivity extends Activity {
         public void onClick(final View v) {
 
             // put comment on currently shown card (if exist).
-            if (Lastcard != null) {
+            if (cur_card != null) {
 
                 // Set up text input
                 final EditText input = new EditText(StudyActivity.this);
@@ -416,7 +457,7 @@ public class StudyActivity extends Activity {
                 input.setGravity(Gravity.LEFT | Gravity.TOP);
                 input.setHorizontalScrollBarEnabled(false); //this
 
-                String comment = Lastcard.Text_of_comments;
+                String comment = cur_card.Text_of_comments;
                 comment += "Comment for revision: ";
                 input.setText(comment);
 
@@ -429,7 +470,7 @@ public class StudyActivity extends Activity {
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 
                             public void onClick(DialogInterface dialog, int whichButton) {
-                                Lastcard.Text_of_comments = input.getText().toString() + "\n\r";
+                                cur_card.Text_of_comments = input.getText().toString() + "\n\r";
                             }//onClick
                         })
                         .setNegativeButton(android.R.string.no, null)
